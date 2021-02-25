82b5315e4d9b78284886a5bddbb08c438c63758e
djbarajas <djbarajas@dons.usfca.edu>
2021-02-24 20:52:23 -0800

    added javadoc to WebCrawler

--- WebCrawler.java
+++ WebCrawler.java
@@ -8,30 +8,55 @@ import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Class responsible for multithreading web crawling.  It fetches urls, parses for links and prints
+ *
+ * @author Daniel Barajas
+ * @version Spring 2021
+ */
 public class WebCrawler {
 
+    /** the urls that have been visited */
     private final Set<String> visited_urls;
+    /** the work queue to multithread */
     private final ExecutorService executor;
     /** string pattern, the pattern to find in the string */
     public static final String TEXT_PATTERN = "(?s)<a[^>]*?href[^>]*?=[^>]*?\"(https?.+?)\".*?>";
     /** the compiled patter for regular expression finding */
     public static final Pattern TEXT_REGEX = Pattern.compile(TEXT_PATTERN);
 
+    /**
+     * Constructor
+     */
     public WebCrawler() {
         this.visited_urls = new HashSet<>();
         this.executor = Executors.newFixedThreadPool(5);
     }
 
+    /**
+     * fetches an html page from a given url
+     *
+     * @param stringUrl the url to connect to
+     * @return the content of the page
+     * @throws IOException if the connection fails
+     */
     public static String fetchContent(String stringUrl) throws IOException {
         URL url = new URL(stringUrl);
         Scanner scanner = new Scanner(url.openStream());
         StringBuilder builder = new StringBuilder();
         while(scanner.hasNext()) {
-            builder.append(scanner.next());
+            builder.append(scanner.next() + System.lineSeparator());
         }
         return builder.toString();
     }
 
+    /**
+     * calls fetch content to get the web page then parses with regular expressions
+     *
+     * @param url the link to fetch page
+     * @return a set of links on the page
+     * @throws IOException if the connection fails
+     */
     public static Set<String> getLinks(String url) throws IOException {
         String content = WebCrawler.fetchContent(url);
         Set<String> localLinks = new HashSet<>();
